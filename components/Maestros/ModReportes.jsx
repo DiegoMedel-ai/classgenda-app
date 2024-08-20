1b759adfe4ca6a7cb35e4271ae96b9e05842a6d8
DiegoMedel-ai <d.i.e.g.o.nambo123@gmail.com>
2024-08-19 21:16:12 -0600

    approach to reports

--- components/Maestros/ModReportes.jsx
+++ components/Maestros/ModReportes.jsx
@@ -1,24 +1,35 @@
-import React, { useState } from "react";
-import { View, Button, StyleSheet, Alert } from "react-native";
-import { Checkbox, Text, Modal, Portal } from "react-native-paper";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Alert,
+  TouchableOpacity,
+  ScrollView,
+  ActivityIndicator
+} from "react-native";
+import { styles as style } from "@/constants/styles";
+import theme from "@/constants/theme";
+import Icon from "react-native-vector-icons/FontAwesome6";
+import { getDateFormat24 } from "@/hooks/date";
+import {
+  Checkbox,
+  Text,
+  Modal,
+  Portal,
+  TextInput as Input,
+  Button,
+} from "react-native-paper";
 
-const CheckboxTree = () => {
-  const [tree, setTree] = useState({
-    "1_nombre": {
-      value: 0,
-      subtemas: {
-        "1.1_nombre": { value: 1, subtemas: {} },
-        "1.2_nombre": { value: 1, subtemas: {} },
-        "1.3_nombre": {
-          value: 0,
-          subtemas: {
-            "1.3.1_nombre": { value: 0, subtemas: {} },
-          },
-        },
-      },
-    },
-    "2_nombre": { value: 0, subtemas: {} },
-  });
+const CheckboxTree = ({ route, navigation }) => {
+  const { materiaNrc } = route.params;
+  const [visibleModal, setVisibleModal] = useState(false);
+  const [subtitleToAdd, setSubtitleToAdd] = useState();
+  const [newPathRef, setNewPathRef] = useState([]);
+  const [nameToAdd, setNameToAdd] = useState("");
+  const [typeOfSub, setTypeOfSub] = useState("tema");
+  const [tree, setTree] = useState({});
+  const [materia, setMateria] = useState()
+  const [loading, setLoading] = useState(false)
 
   const handleCheckboxChange = (path) => {
     const updatedTree = { ...tree };
@@ -43,13 +54,11 @@ const CheckboxTree = () => {
     const updatedTree = { ...tree };
     let current = updatedTree;
 
-    // Navega hasta el nodo padre
     path.forEach((key, index) => {
       if (index === path.length - 1) {
-        // Crear el nuevo subtema dentro del nodo padre
         const subtemaCount = Object.keys(current[key].subtemas).length + 1;
         const parentKey = key.split("_")[0];
-        const newSubtema = `${parentKey}.${subtemaCount}_nombre`;
+        const newSubtema = `${parentKey}.${subtemaCount}_${nameToAdd}`;
         current[key].subtemas[newSubtema] = { value: 0, subtemas: {} };
       } else {
         if (!current[key]) {
@@ -59,54 +68,46 @@ const CheckboxTree = () => {
       }
     });
 
+    setVisibleModal(false);
     setTree(updatedTree);
+  };
+
+  const getNewCount = (path) => {
+    const updatedTree = { ...tree };
+    let current = updatedTree;
+
+    if (!path) {
+      setSubtitleToAdd(Object.keys(current).length + 1);
+      setVisibleModal(true);
+    } else {
+      path.forEach((key, index) => {
+        if (index === path.length - 1) {
+          const subtemaCount = Object.keys(current[key].subtemas).length + 1;
+          const parentKey = key.split("_")[0];
+          const newSubtema = `${parentKey}.${subtemaCount}`;
+          setSubtitleToAdd(newSubtema);
+          setNewPathRef(path);
+          setVisibleModal(true);
+        } else {
+          if (!current[key]) {
+            current[key] = { value: 0, subtemas: {} };
+          }
+          current = current[key].subtemas;
+        }
+      });
+    }
   };
 
   const handleAddTema = () => {
     const updatedTree = { ...tree };
     let current = updatedTree;
 
-    const temaCount = Object.keys(current).length +1
+    const temaCount = Object.keys(current).length + 1;
+    current[`${temaCount}_${nameToAdd}`] = { value: 0, subtemas: {} };
 
-    // Navega hasta el nodo padre
-    path.forEach((key, index) => {
-      if (index === path.length - 1) {
-        // Crear el nuevo subtema dentro del nodo padre
-        const subtemaCount = Object.keys(current[key].subtemas).length + 1;
-        const parentKey = key.split("_")[0];
-        const newSubtema = `${parentKey}.${subtemaCount}_nombre`;
-        current[key].subtemas[newSubtema] = { value: 0, subtemas: {} };
-      } else {
-        if (!current[key]) {
-          current[key] = { value: 0, subtemas: {} };
-        }
-        current = current[key].subtemas;
-      }
-    });
-
+    setVisibleModal(false);
     setTree(updatedTree);
   };
-
-  const handleAlert = () => {
-    console.log('2');
-    
-    Alert.prompt(
-        "Enter password",
-        "Enter your password to claim your $1.5B in lottery winnings",
-        [
-          {
-            text: "Cancel",
-            onPress: () => console.log("Cancel Pressed"),
-            style: "cancel"
-          },
-          {
-            text: "OK",
-            onPress: password => console.log("OK Pressed, password: " + password)
-          }
-        ],
-        "secure-text"
-      );
-  }
 
   const renderTree = (node, path = []) => {
     return Object.keys(node).map((key) => {
@@ -115,45 +116,255 @@ const CheckboxTree = () => {
         <View key={newPath.join("_")} style={{ paddingLeft: path.length * 20 }}>
           <View style={styles.checkboxRow}>
             <Checkbox
+              color="green"
               status={node[key].value === 1 ? "checked" : "unchecked"}
               onPress={() => handleCheckboxChange(newPath)}
             />
-            <Text>{key.replace(/_/g, ".")}</Text>
-            <Button title="+" onPress={() => handleAddSubtema(newPath)} />
+            <Text style={{ fontSize: 15 }}>{key.replace(/_/g, ".")}</Text>
           </View>
-          {node[key].subtemas && renderTree(node[key].subtemas, newPath)}
+          <TouchableOpacity
+            onPress={() => {
+              setTypeOfSub("sub");
+              getNewCount(newPath);
+            }}
+            style={{ height: 30, paddingVertical: 0, justifyContent: "center" }}
+          >
+            <Text
+              style={{
+                fontSize: 11,
+                width: "auto",
+                paddingLeft: 40,
+                color: theme.colors.tertiary,
+              }}
+            >
+              + Añadir subtema
+            </Text>
+          </TouchableOpacity>
+          {node[key].subtemas &&
+            node[key].value === 0 &&
+            renderTree(node[key].subtemas, newPath)}
         </View>
       );
     });
   };
 
-  const handleSave = () => {
-    console.log(tree);
+  const fetchMateria = () => {
+    setLoading(true);
+
+    try {
+      const options = {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+        },
+      };
+
+      const url = `${process.env.EXPO_PUBLIC_API_URL}/materias/${materiaNrc}`;
+      fetch(url, options)
+        .then((response) => response.json())
+        .then((data) => {
+          setMateria(data);
+          setTree(JSON.parse(data.temas))
+        })
+        .then(() => setLoading(false))
+        .catch((error) => {
+          console.log(
+            `Fetch error to: ${process.env.EXPO_PUBLIC_API_URL}/materias`,
+            error
+          );
+        });
+    } catch (error) {}
   };
 
+  const updateTemas = () => {    
+    setLoading(true);
+
+    const data = {...materia}
+    data.profesor = data.profesor.id
+    data.programa = data.programa.clave
+
+    try {
+      const options = {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({...data, temas: JSON.stringify(tree)})
+      };
+
+      console.log(JSON.stringify({...data, temas: JSON.stringify(tree)}));
+      
+
+      const url = `${process.env.EXPO_PUBLIC_API_URL}/materias/update`;
+      fetch(url, options)
+        .then((response) => {
+          if(response.status !== 200) {
+            Alert.alert('Error', `Ocurrió un error inesperado`, [
+              {
+                "text": "Ok",
+                "style": "cancel"
+              }
+            ])
+          }
+        })
+        .then(() => setLoading(false))
+        .catch((error) => {
+          console.log(
+            `Fetch error to: ${process.env.EXPO_PUBLIC_API_URL}/materias`,
+            error
+          );
+        });
+    } catch (error) {}
+  }
+
+  useEffect(() => {
+    fetchMateria()
+  }, [])
+
+  const [firstFetch, setFirstFetch] = useState(true)
+  
+  useEffect(() => {
+    if(materia){
+      if(firstFetch){
+        setFirstFetch(false);
+      }else{
+        updateTemas()
+      }
+    }
+  }, [tree])
+  
+
   return (
-    <View style={styles.container}>
-      {renderTree(tree)}
-      <Button title="Guardar" onPress={handleAlert} />
-      <Portal>
-        <Modal
-          visible={visibleModal}
-          onDismiss={() => setVisibleModal(false)}
-          contentContainerStyle={{
-            backgroundColor: "white",
-            padding: 30,
-            width: "70%",
-            margin: "auto",
-            borderRadius: 30,
+    <View style={{ ...style.general.overlay_top }}>
+      <View
+        style={{
+          height: 220,
+          width: "100%",
+          backgroundColor: theme.colors.secondary_op,
+          borderBottomLeftRadius: 40,
+          borderBottomRightRadius: 40,
+          flexDirection: "row",
+          elevation: 5,
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 5 },
+          shadowOpacity: 0.3,
+          shadowRadius: 5,
+        }}
+      >
+        {materia &&
+        <View style={{justifyContent: 'top', alignItems: 'center', width: '100%'}}>
+          <Text style={{width: 'auto', fontSize: 24, marginTop: 20}}>Reportar</Text>
+          <Text style={{width: 'auto', fontSize: 20, marginTop: 20}}>{materia.programa.nombre}</Text>
+          <Text style={{width: 'auto', fontSize: 16, marginTop: 25}}>NRC: {materia.nrc}</Text>
+          <View style={{flexDirection: 'row', width: '70%', justifyContent: 'space-around'}}>
+          <Text style={{width: 'auto', fontSize: 14, marginTop: 15}}>Clave: {materia.programa.clave}</Text>
+          <Text style={{width: 'auto', fontSize: 14, marginTop: 15}}>{getDateFormat24(materia.hora_inicio)} - {getDateFormat24(materia.hora_final)}</Text>
+          <Text style={{width: 'auto', fontSize: 14, marginTop: 15}}>Aula: {materia.edificio}-{materia.aula}</Text>
+
+          </View>
+        </View>
+        }
+      </View>
+      <View style={{ padding: 20, paddingVertical: 30, width: "100%" }}>
+        <View style={styles.container}>
+          <ScrollView style={{ maxHeight: 300 }}>{renderTree(tree)}</ScrollView>
+          <TouchableOpacity
+            onPress={() => {
+              setTypeOfSub("tema");
+              getNewCount();
+            }}
+            style={{ height: 40, paddingVertical: 0, justifyContent: "center" }}
+          >
+            <Text
+              style={{
+                fontSize: 11,
+                width: "auto",
+                color: theme.colors.tertiary,
+              }}
+            >
+              + Añadir tema
+            </Text>
+          </TouchableOpacity>
+          <Portal>
+            <Modal
+              visible={visibleModal}
+              onDismiss={() => setVisibleModal(false)}
+              contentContainerStyle={{
+                backgroundColor: "white",
+                padding: 30,
+                width: "90%",
+                margin: "auto",
+                borderRadius: 30,
+              }}
+            >
+              <View style={{ alignItems: "center" }}>
+                <Text
+                  style={{
+                    textAlign: "center",
+                    marginBottom: 15,
+                    fontSize: 20,
+                  }}
+                >
+                  Nombre del tema {subtitleToAdd}
+                </Text>
+                <Input
+                  onChangeText={(text) => setNameToAdd(text)}
+                  style={{
+                    ...style.login.input_text,
+                    width: "90%",
+                    marginVertical: 10,
+                  }}
+                  theme={{ roundness: 20 }}
+                  activeOutlineColor={"black"}
+                  mode="outlined"
+                />
+                <Button
+                  onPress={() =>
+                    typeOfSub.includes("tema")
+                      ? handleAddTema()
+                      : handleAddSubtema(newPathRef)
+                  }
+                  style={{
+                    backgroundColor: theme.colors.tertiary_op,
+                    marginTop: 15,
+                  }}
+                  mode="elevated"
+                >
+                  <Text style={{ color: "black" }}>Añadir</Text>
+                </Button>
+              </View>
+            </Modal>
+          </Portal>
+        </View>
+        <View style={{paddingTop: 20}}>
+        <Button
+          mode="elevated"
+          icon={() => <Icon name="file" color="black" size={22} />}
+          contentStyle={{ flexDirection: "row-reverse" }}
+          style={{
+            ...style.general.button,
+            ...style.general.center,
+            marginHorizontal: 'auto',
+            backgroundColor: theme.colors.tertiary,
+            marginTop: 0,
+            width: 'auto'
           }}
+          onPressIn={() => navigation.navigate("ModReportes", {materiaNrc: materiaNrc})}
         >
-            <View style={{ alignItems: "center" }}>
-              <Text style={{ textAlign: "center", marginTop: 10 }}>
-                Hubo un error
-              </Text>
-            </View>
-        </Modal>
-      </Portal>
+          <Text style={{paddingHorizontal: 10, fontSize: 15}}>
+            Crear reporte
+          </Text>
+        </Button>
+        </View>
+      </View>
+      
+      {loading && (
+        <View style={style.general.overlay_loader}>
+          <ActivityIndicator size={"large"} color={"#4DBFE4"} />
+        </View>
+      )}
     </View>
   );
 };
@@ -161,8 +372,11 @@ const CheckboxTree = () => {
 const styles = StyleSheet.create({
   container: {
     padding: 20,
-    backgroundColor: "#F8DCDC",
+
+    backgroundColor: theme.colors.secondary_op,
     borderRadius: 10,
+    width: "95%",
+    marginHorizontal: "auto",
   },
   checkboxRow: {
     flexDirection: "row",
